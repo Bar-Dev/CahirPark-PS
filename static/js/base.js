a90a72f75bab55a8109e9fcb1b66fe3271ff6142
Bar-Dev <bdprojectservices@gmail.com>
2023-09-27 08:43:44 +0000

    fixed issue with dropdown not staying open

--- static/js/base.js
+++ static/js/base.js
@@ -47,7 +47,44 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-document.getElementById("myDropdown").addEventListener("click", function(e) {
 
-  e.stopPropagation();
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the products dropdown and submenu
+  const productsDropdown = document.getElementById("productsDropdown");
+  const productsSubmenu = document.getElementById("productsSubmenu");
+
+  // Check if the submenu should be shown initially
+  const shouldShowSubmenu = sessionStorage.getItem("showProductsSubmenu") === "true";
+
+  // Toggle the submenu visibility based on the stored state
+  if (shouldShowSubmenu) {
+      productsSubmenu.classList.add("show");
+      productsDropdown.setAttribute("aria-expanded", "true");
+  }
+
+  // Add a click event listener to the products dropdown link
+  productsDropdown.addEventListener("click", function (e) {
+      e.preventDefault();
+      const isSubmenuShown = productsSubmenu.classList.contains("show");
+
+      if (!isSubmenuShown) {
+          // Store the submenu state as open
+          sessionStorage.setItem("showProductsSubmenu", "true");
+          productsSubmenu.classList.add("show");
+          productsDropdown.setAttribute("aria-expanded", "true");
+      } else {
+          // If the submenu is already open and the Products link is clicked again, close the submenu
+          sessionStorage.setItem("showProductsSubmenu", "false");
+          productsSubmenu.classList.remove("show");
+          productsDropdown.setAttribute("aria-expanded", "false");
+          // Reload the page to reset the dropdown state
+          window.location.reload();
+      }
+  });
 });
+
+
+
+
+

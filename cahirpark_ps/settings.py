2beb52b4747d8196fded988b83572b761194fae2
Bar-Dev <bdprojectservices@gmail.com>
2023-09-27 16:22:26 +0000

    dry commit

--- cahirpark_ps/settings.py
+++ cahirpark_ps/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from django.utils.translation import gettext_lazy as _
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'products',
     'about_the_pro',
     'bag',
-    'djmoney',
 ]
 
 MIDDLEWARE = [
